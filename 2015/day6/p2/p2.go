ac57af9a003a932fe1a3fba2c1d8300f57e6db49
fahlmant <fahlmantaylor@gmail.com>
2019-11-28 17:40:06 -0500

    2015 Day 6

---
+++ 2015/day6/p2/p2.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	var grid [1000][1000]int
+
+	file, err := os.Open("../input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+		if split[0] == "toggle" {
+			toggle(&grid, split)
+		} else if split[0] == "turn" {
+			turn(&grid, split)
+		}
+	}
+
+	total := 0
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			total += grid[i][j]
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func toggle(grid *[1000][1000]int, line []string) {
+
+	xy1 := strings.Split(line[1], ",")
+	xy2 := strings.Split(line[3], ",")
+
+	x1, _ := strconv.Atoi(xy1[0])
+	x2, _ := strconv.Atoi(xy2[0])
+	y1, _ := strconv.Atoi(xy1[1])
+	y2, _ := strconv.Atoi(xy2[1])
+
+	for i := x1; i <= x2; i++ {
+		for j := y1; j <= y2; j++ {
+			grid[i][j] += 2
+		}
+	}
+}
+
+func turn(grid *[1000][1000]int, line []string) {
+
+	var boolValue bool
+
+	if line[1] == "on" {
+		boolValue = true
+	} else if line[1] == "off" {
+		boolValue = false
+	}
+
+	xy1 := strings.Split(line[2], ",")
+	xy2 := strings.Split(line[4], ",")
+
+	x1, _ := strconv.Atoi(xy1[0])
+	x2, _ := strconv.Atoi(xy2[0])
+	y1, _ := strconv.Atoi(xy1[1])
+	y2, _ := strconv.Atoi(xy2[1])
+
+	for i := x1; i <= x2; i++ {
+		for j := y1; j <= y2; j++ {
+			if boolValue {
+				grid[i][j]++
+			} else {
+				grid[i][j]--
+				if grid[i][j] < 0 {
+					grid[i][j] = 0
+				}
+			}
+		}
+	}
+
+}
