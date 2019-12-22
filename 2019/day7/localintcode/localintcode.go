1a7a24a7372dff6e9fecfa65245d280bdff30223
Taylor Fahlman <tfahlman@redhat.com>
2019-12-22 13:08:45 -0500

    Debug local intcode

--- 2019/day7/localintcode/localintcode.go
+++ 2019/day7/localintcode/localintcode.go
@@ -13,40 +13,57 @@ type Computer struct {
 func (c *Computer) RunProgram() {
 
 	for {
+		fmt.Println(c.PC)
 		instruction := c.Instructions[c.PC]
 		opcode, arg1, arg2, arg3 := c.parseOpcode(instruction)
-
+		fmt.Printf("PC: %d\n", c.PC)
 		switch opcode {
 		case 1:
+			fmt.Println("Opcode 1")
+			fmt.Printf("Adding memaddr %d value %d with memaddr %d value %d at address %d, currently holding %d\n", arg1, c.Instructions[arg1], arg2, c.Instructions[arg2], arg3, c.Instructions[arg3])
 			c.Instructions[arg3] = c.Instructions[arg1] + c.Instructions[arg2]
+			fmt.Printf("Address %d now is %d\n", arg3, c.Instructions[arg3])
 			c.PC += 4
 		case 2:
+			fmt.Println("Opcode 2")
 			c.Instructions[arg3] = c.Instructions[arg1] * c.Instructions[arg2]
 			c.PC += 4
 		case 3:
+			fmt.Println("Opcode 3")
 			if len(c.Input) >= 1 {
 				input := c.Input[0]
 				c.Input = c.Input[1:]
+				fmt.Printf("Placing input value %d at address %d, currently holding %d\n", input, arg1, c.Instructions[arg1])
 				c.Instructions[arg1] = input
+				fmt.Printf("Address %d now is %d\n", arg1, c.Instructions[arg1])
+			} else {
+
+				return
 			}
 			c.PC += 2
 		case 4:
+			fmt.Println("Opcode 4")
 			c.Output = append(c.Output, c.Instructions[arg1])
 			c.PC += 2
 			return
 		case 5:
+			fmt.Println("Opcode 5")
+			fmt.Printf("Jump if non zero, addr %d value %d\n", arg1, c.Instructions[arg1])
 			if c.Instructions[arg1] != 0 {
+				fmt.Printf("Setting PC to %d from address %d\n", c.Instructions[arg2], arg2)
 				c.PC = c.Instructions[arg2]
 			} else {
 				c.PC += 3
 			}
 		case 6:
+			fmt.Println("Opcode 6")
 			if c.Instructions[arg1] == 0 {
 				c.PC = c.Instructions[arg2]
 			} else {
 				c.PC += 3
 			}
 		case 7:
+			fmt.Println("Opcode 7")
 			if c.Instructions[arg1] < c.Instructions[arg2] {
 				c.Instructions[arg3] = 1
 			} else {
@@ -54,6 +71,7 @@ func (c *Computer) RunProgram() {
 			}
 			c.PC += 4
 		case 8:
+			fmt.Println("Opcode 8")
 			if c.Instructions[arg1] == c.Instructions[arg2] {
 				c.Instructions[arg3] = 1
 			} else {
@@ -61,6 +79,7 @@ func (c *Computer) RunProgram() {
 			}
 			c.PC += 4
 		case 9:
+			fmt.Println("Opcode 9")
 			c.Offset += c.Instructions[arg1]
 			c.PC += 2
 		case 99:
@@ -69,6 +88,7 @@ func (c *Computer) RunProgram() {
 			fmt.Printf("Invalid Opcode: %d\n", opcode)
 			return
 		}
+		fmt.Println(c.Instructions)
 	}
 }
 
@@ -95,12 +115,11 @@ func (c *Computer) parseOpcode(instruction int) (int, int, int, int) {
 	if opcode != 99 {
 		arg1 = c.Instructions[c.PC+1]
 		//9 only has 1 argument
-		if opcode != 9 {
+		if !inSlice([]int{3, 4, 9}, opcode) {
 			arg2 = c.Instructions[c.PC+2]
 			//3 and 4 only have 2 arguments
-			if !inSlice([]int{3, 4}, opcode) {
-				arg3 = c.Instructions[c.PC+3]
-			}
+
+			arg3 = c.Instructions[c.PC+3]
 		}
 	}
 
