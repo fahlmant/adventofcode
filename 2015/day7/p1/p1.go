213e0074cfc7964d7888d9f192c9d23e9976b9ea
Taylor Fahlman <tfahlman@redhat.com>
2020-11-28 09:55:27 -0500

    getting closer

--- 2015/day7/p1/p1.go
+++ 2015/day7/p1/p1.go
@@ -5,22 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
-type Circut struct {
+type instruction struct {
 	output    string
 	inputs    []string
-	values    []int
 	operation string
+}
+
+type wires struct {
+	wireMap map[string]int
 }
 
 func main() {
 
-	circuts := make(map[string]*Circut)
-
+	allWires := wires{wireMap: make(map[string]int, 1)}
+	var instructions []instruction
 	file, err := os.Open("../input")
 	if err != nil {
 		log.Fatal(err)
@@ -30,36 +32,100 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		circut := parseInstruction(line)
-		circuts[circut.output] = circut
+		instructions = append(instructions, parseInstruction(line))
 	}
 
-	fmt.Println(circuts)
+	allWires.runCircuts(instructions)
+	fmt.Printf("%+v\n", allWires.wireMap)
 }
 
-func parseInstruction(instruction string) *Circut {
+func parseInstruction(line string) instruction {
 
-	operationRegex := regexp.MustCompile("[A-Z]+")
-	outputRegex := regexp.MustCompile("-> (.+)")
-	inputRegex := regexp.MustCompile("[a-z0-9]+")
-	wireRegex := regexp.MustCompile("[a-z]+")
+	ins := instruction{}
 
-	operation := operationRegex.FindString(instruction)
-	output := outputRegex.FindString(instruction)
-	inputs := inputRegex.FindAllStringSubmatch(instruction, -1)
+	// Split on -> operator
+	splitInput := strings.Split(line, "->")
 
-	var wireInputs []string
-	var values []int
+	// Output register is after ->
+	ins.output = strings.TrimSpace(splitInput[1])
 
-	for _, input := range inputs {
-		if wireRegex.MatchString(input[0]) {
-			wireInputs = append(wireInputs, input[0])
+	splitLeft := strings.Split(splitInput[0], " ")
+	// If there's more than one word on the left side, parse it out
+	if splitLeft[1] == "AND" || splitLeft[1] == "OR" || splitLeft[1] == "LSHIFT" || splitLeft[1] == "RSHIFT" {
+		ins.operation = strings.TrimSpace(splitLeft[1])
+		ins.inputs = append(ins.inputs, strings.TrimSpace(splitLeft[0]))
+		ins.inputs = append(ins.inputs, strings.TrimSpace(splitLeft[2]))
+	} else if splitLeft[0] == "NOT" {
+		ins.operation = "NOT"
+		ins.inputs = append(ins.inputs, strings.TrimSpace(splitLeft[1]))
+	} else {
+		// Use PLACE for the operation with no keyword
+		ins.operation = "PLACE"
+		ins.inputs = append(ins.inputs, strings.TrimSpace(splitLeft[0]))
+	}
+
+	return ins
+}
+
+func (w wires) executeInstruction(ins instruction) {
+
+	var intInputs []int
+	for _, input := range ins.inputs {
+		input = strings.TrimSpace(input)
+		if num, err := strconv.Atoi(input); err == nil {
+			intInputs = append(intInputs, num)
 		} else {
-			value, _ := strconv.Atoi(input[0])
-			values = append(values, value)
+			intInputs = append(intInputs, w.wireMap[input])
 		}
 	}
 
-	outputWire := strings.Split(output, " ")[1]
-	return &Circut{outputWire, wireInputs[0 : len(wireInputs)-1], values, operation}
+	//fmt.Printf("executing instruction %+v with inputs %+v\n", ins, intInputs)
+
+	switch ins.operation {
+	case "AND":
+		w.wireMap[ins.output] = intInputs[0] & intInputs[1]
+	case "OR":
+		w.wireMap[ins.output] = intInputs[0] | intInputs[1]
+	case "LSHIFT":
+		w.wireMap[ins.output] = intInputs[0] << uint(intInputs[1])
+	case "RSHIFT":
+		w.wireMap[ins.output] = intInputs[0] >> uint(intInputs[1])
+	case "NOT":
+		w.wireMap[ins.output] = intInputs[0]
+	case "PLACE":
+		w.wireMap[ins.output] = intInputs[0]
+	default:
+		panic("Error")
+	}
+
+}
+
+func (w wires) runCircuts(instructions []instruction) {
+
+	var instructionDone []instruction
+	for len(instructions) != len(instructionDone) {
+		for _, ins := range instructions {
+			//fmt.Printf("%+v\n", ins)
+			if !w.validateInstruction(ins) {
+				continue
+			}
+			w.executeInstruction(ins)
+			instructionDone = append(instructionDone, ins)
+		}
+	}
+}
+
+func (w wires) validateInstruction(ins instruction) bool {
+
+	for _, input := range ins.inputs {
+		input = strings.TrimSpace(input)
+		if _, err := strconv.Atoi(input); err != nil {
+			if _, ok := w.wireMap[input]; !ok {
+				fmt.Printf("%+v\n", ins.inputs)
+				fmt.Printf("Not valid because %s has no value\n", input)
+				return false
+			}
+		}
+	}
+	return true
 }
