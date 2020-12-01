c024a5052d505e173600b345dfd5d3664cb0e39d
Taylor Fahlman <tfahlman@redhat.com>
2020-12-01 06:53:50 -0500

    Advent of Code 2020 Day 1 (Rust)

---
+++ 2020/day1/p2/p2.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn main() {
+
+    // Create a mutable vector of i32
+    let mut v: Vec<i32> = Vec::new();
+
+    // Use fs to get all input to a Result<String>, which is then unwrapped
+    let contents = fs::read_to_string("../input").unwrap();
+
+    // For every enumerated line (split \n)
+    for number in contents.trim().split('\n').enumerate() {
+        // enumerate here returns (usize, &str), so
+        // taking number.1 gets the &str, then its parsed into
+        // a Result<i32>, which is unwrapped and pushed into the vector v
+        v.push(number.1.parse::<i32>().unwrap())
+    }   
+
+    // Iterate through each element of the vector with iter and enumerate
+    for (i, number) in v.iter().enumerate() {
+        // Nested iteration for comparison
+        for (j, number2) in v.iter().enumerate() {
+            for (k, number3) in v.iter().enumerate() {
+                // Don't check the same index
+                if i == j || i == k || j == k{
+                    continue;
+                }
+                // If all three numbers add to 2020, print the product of those numbers and be done
+                if (number + number2 + number3) == 2020 {
+                    let answer = number * number2 * number3;
+                    println!("{}", answer);
+                    return
+                }
+            }
+        }
+    }
+}
