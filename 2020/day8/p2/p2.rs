ca4fba441a51e425d2c6f1d628dbeeb83c586146
Taylor Fahlman <tfahlman@redhat.com>
2020-12-09 13:32:12 -0500

    Advent of Code 2020 Day 8 Pt 2 (Rust)

--- 2020/day8/p2/p2.rs
+++ 2020/day8/p2/p2.rs
@@ -13,12 +13,27 @@ impl GameConsole {
         let ins: Vec<&str> = self.instructions[self.ic].split(" ").collect();
 
         let sign = ins[1].chars().nth(0).unwrap().to_string();
-        let mut value = ins[1][1..].to_string().parse::<usize>().unwrap();
+        let value = ins[1][1..].to_string().parse::<usize>().unwrap();
         match ins[0] {
             "acc" => {if sign == "-"{self.accumulator -= value} else{self.accumulator+= value}; self.ic = self.ic + 1;}
             "jmp" => if sign == "-"{self.ic -= value} else{self.ic+= value},
             "nop" => self.ic = self.ic + 1,
             _ => return,
+        }
+    }
+
+    fn run_program(&mut self) -> Result<usize, String> {
+        let mut ic_tracker: HashSet<usize> = HashSet::new();
+        loop {
+            if self.ic >= self.instructions.len() {
+                return Ok(self.accumulator);
+            }
+            if ic_tracker.contains(&self.ic) {
+                return Err("Infinite Loop".to_string());
+            }
+            
+            ic_tracker.insert(self.ic);
+            &self.run_instruction();
         }
     }
 }
@@ -29,23 +44,29 @@ fn main() {
 
     let lines: Vec<String> = contents.trim().lines().map(|l| l.to_string()).collect();
 
-    let mut game_console: GameConsole = GameConsole{
+    let game_console: GameConsole = GameConsole{
         instructions: lines,
         accumulator: 0,
         ic: 0,
     };
 
     for i in 0..game_console.instructions.len() {
-        let game = game_console.clone();
-    }
+        let mut game = GameConsole {
+            instructions: game_console.instructions.clone(),
+            accumulator: 0,
+            ic: 0,
+        };
 
-    loop {
-        println!("Running instruction");
-        if game_console.ic >= game_console.instructions.len() {
-            break;
+        if game.instructions[i].contains("jmp") {
+            game.instructions[i] = game.instructions[i].replace("jmp", "nop");
+        } else if game.instructions[i].contains("nop") {
+            game.instructions[i] = game.instructions[i].replace("nop", "jmp");
         }
-        game_console.run_instruction();
+
+        let result = game.run_program();
+        match result {
+            Ok(answer) => {println!("{}", answer); break;},
+            Err(_) => continue,
+        }
     }
-    
-    println!("{}", game_console.accumulator);
 }
