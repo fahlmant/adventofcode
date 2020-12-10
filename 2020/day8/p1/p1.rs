db7857993a4de017502189ace142eb2a680edc74
fahlmant <fahlmantaylor@gmail.com>
2020-12-10 06:36:55 -0500

    Small fix to day 8 2020

--- 2020/day8/p1/p1.rs
+++ 2020/day8/p1/p1.rs
@@ -13,7 +13,7 @@ impl GameConsole {
         let ins: Vec<&str> = self.instructions[self.ic].split(" ").collect();
 
         let sign = ins[1].chars().nth(0).unwrap().to_string();
-        let mut value = ins[1][1..].to_string().parse::<usize>().unwrap();
+        let value = ins[1][1..].to_string().parse::<usize>().unwrap();
         match ins[0] {
             "acc" => {if sign == "-"{self.accumulator -= value} else{self.accumulator+= value}; self.ic = self.ic + 1;}
             "jmp" => if sign == "-"{self.ic -= value} else{self.ic+= value},
