7ba34b4083f54b22d5a72a2219fb3b9eee3a5f4a
Taylor Fahlman <tfahlman@redhat.com>
2024-12-20 06:50:44 -0800

    2024 Day 19 pt1

---
+++ 2024/day19/p1/p1.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	total := 0
+	file, err := os.ReadFile("../input")
+	if err != nil {
+		panic(err)
+	}
+
+	input := strings.Split(string(file), "\n\n")
+
+	towels := strings.Split(input[0], ", ")
+
+	desiredDesigns := strings.Split(input[1], "\n")
+
+	for _, d := range desiredDesigns {
+		if ok := isDesignPossible(towels, d); ok {
+			total += 1
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func isDesignPossible(towels []string, design string) bool {
+
+	var towelsAsBytes [][]byte
+
+	for _, t := range towels {
+		localTowel := make([]byte, len(t))
+		copy(localTowel[:], t)
+		towelsAsBytes = append(towelsAsBytes, localTowel)
+	}
+
+	localDesign := make([]byte, len(design))
+	copy(localDesign[:], []byte(design))
+
+	cache := make(map[string]bool)
+	for _, t := range towels {
+		cache[t] = true
+	}
+
+	isPossible := designCheck(towelsAsBytes, localDesign, cache, 0)
+
+	return isPossible
+}
+
+func designCheck(towels [][]byte, target []byte, cache map[string]bool, depth int) bool {
+
+	// If we already know the target (all of the rest of the string) is possible, don't check
+	if b, exists := cache[string(target)]; exists {
+		return b
+	}
+
+	// If the target is exauhsted, we've checked everything
+	if len(target) == 0 {
+		return true
+	}
+
+	var areAnyPossible []bool
+	for _, t := range towels {
+		if len(t) > len(target) {
+			continue
+		}
+		if strings.HasPrefix(string(target), string(t)) {
+			result := designCheck(towels, target[len(t):], cache, depth+1)
+			areAnyPossible = append(areAnyPossible, result)
+
+			if result {
+				cache[string(target)] = true
+				return true
+			}
+		}
+	}
+
+	cache[string(target)] = false
+	return false
+}
